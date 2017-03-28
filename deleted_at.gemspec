8f70713fade04d780f880179fdb3f10be24b0515
Dale Stevens <dale@twilightcoders.net>
2017-03-28 14:40:32 -0600

    Improve how DeletedAt is loaded

--- deleted_at.gemspec
+++ deleted_at.gemspec
@@ -31,6 +31,8 @@ Gem::Specification.new do |spec|
   spec.required_ruby_version = '>= 2.0'
 
   spec.add_dependency "activerecord", "~> 4.2"
+  spec.add_dependency "railties", "~> 4.2"
+  spec.add_dependency "scenic", "~> 1.3"
   spec.add_dependency 'request_store', '~> 1.1', '>= 1.1'
 
   spec.add_development_dependency "bundler", "~> 1.13"
--- lib/deleted_at/railtie.rb
+++ lib/deleted_at/railtie.rb
@@ -1,16 +1,13 @@
 require 'rails/railtie'
-require 'action_controller'
-require 'action_controller/railtie'
 require 'deleted_at/active_record/base'
 require 'deleted_at/active_record/relation'
 
 module DeletedAt
-  class Railtie < ::Rails::Railtie
+  class Railtie < Rails::Railtie
 
-    initializer 'deleted_at.install' do
+    initializer 'deleted_at.load' do
       ActiveSupport.on_load(:active_record) do
-        ::ActiveRecord::Relation.send :include, DeletedAt::ActiveRecord::Relation
-        ::ActiveRecord::Base.send :include, DeletedAt::ActiveRecord::Base
+        DeletedAt.load
       end
     end
 
