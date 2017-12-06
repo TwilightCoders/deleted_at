590ebfc8a8de686da78fc27e7d924872ce55e0b9
Dale Stevens <dale@twilightcoders.net>
2017-12-06 12:04:40 -0700

    Add database cleanup to suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,4 +14,15 @@ Combustion.initialize! :active_record
 RSpec.configure do |config|
   config.order = 'random'
 
+  # Configure the DatabaseCleaner
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
